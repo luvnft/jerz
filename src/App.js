cd46720d8639def56b9c407600670c5e7490ea93
rishab <rishavraj.verma@indevconsultancy.in>
2025-01-15 10:50:51 +0530

    installing and setup react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+const App = () => {
+  return (
+    <>
+      <div className="bg-gray-50 min-h-[90vh]">
+        <div className="min-h-80 flex justify-center">hello</div>
+      </div>
+    </>
+  );
+};
+
+export default App;
